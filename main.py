0edf0140a432a5b07aee9d9a337c4ba3a5b731cf
ELITENICK <nikita_shimenkov2011@mail.ru>
2024-09-16 00:03:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from datetime import datetime
 from time import sleep
 
 # To obtain API Keys, you have to visit https://my.telegram.org/apps, and make an app.
-API_ID = 0  # ENTER YOU API ID HERE (SHOUlD BE AN INT)
-API_HASH = '<ENTER YOUR API HASH HERE>'
+API_ID = 12345678  # ENTER YOU API ID HERE (SHOUlD BE AN INT)
+API_HASH = '123456789'
 
 USERNAMES = [
+    'your_username'
     # enter all username you wish to track here
-    # like 'aswin_shenoy'
+    # like 'aswin_shenoy' or
+    # 'aswin_shenoy',
+    # 'aswin_shenoy2'
 ]
 
 PAIRS = [
@@ -20,8 +23,8 @@ PAIRS = [
 
 # Sleep time before each instance of tracking
 # if sleep time is too low, a 24hr lock with telethon.errors.rpcerrorlist.FloodWaitError will be hit
-# ^ @todo fix this
-TRACK_FREQUENCY = 15
+# Default - 10
+TRACK_FREQUENCY = 5
 
 client = TelegramClient('bot', API_ID, API_HASH)
 
@@ -37,7 +40,7 @@ async def main():
             name = entity.first_name + ' ' + entity.last_name
             if isinstance(entity.status, UserStatusOnline):
                 # print if user tracked is online
-                print('[' + datetime.now().isoformat() + ']: ' + name + ' is online')
+                print('[' + datetime.now().isoformat() + ']: ' + name + ' в онлайне!')
                 online.append(entity.username)
             else:
                 offline.append(entity.username)
@@ -46,16 +49,16 @@ async def main():
         for u in PAIRS:
             # check if both users in the given pair are online right now
             if u[0] in online and u[1] in online:
-                print('[' + datetime.now().isoformat() + ']: ' + u[0] + ' & ' + u[1] + ' are both online right now')
+                print('[' + datetime.now().isoformat() + ']: ' + u[0] + ' и ' + u[1] + ' оба сейчас находятся в онлайне!')
             elif u[0] in offline and u[1] in offline:
-                print('[' + datetime.now().isoformat() + ']: ' + u[0] + ' & ' + u[1] + ' are both offline right now')
+                print('[' + datetime.now().isoformat() + ']: ' + u[0] + ' и ' + u[1] + ' оба сейчас находятся в офлайне!')
 
         # sleep until provided seconds
         sleep(TRACK_FREQUENCY)
 
 if __name__ == "__main__":
-    print("Tracker Started")
+    print("Трекер начал работу...")
     client.start()
-    print("Tracker successfully connected to Telegram.")
+    print("Трекер успешно подключился к Telegram'у!")
     with client:
         client.loop.run_until_complete(main())
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+color 03
+title ● PostAai (aka Telegram Online Tracker)
+python3.10 main.py
+pause
